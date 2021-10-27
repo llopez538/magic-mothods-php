d394351b8d7c59990df6319ba73ca2ef49ee5dac
Leonard Lopez <masasss.the.end@gmail.com>
2021-10-27 02:25:37 -0400

    magic methods

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
 
+use Styde\User;
+
 require('../vendor/autoload.php');
+
+$user = new User();
+
+$user->fill([
+    'first_name' => 'Leonard',
+    'last_name' => 'López Molina'
+]);
+
+$user->nickname = 'pollito_123';
+
+unset($user->nickname);
+
+echo "<h1>Bienvenido a los metodos magicos con: $user->first_name $user->last_name</h1>";
+
+if (isset($user->nickname)) {
+    echo "<p>Nickname: $user->nickname</p>";
+}
